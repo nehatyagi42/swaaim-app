438b156d4299b47e82f708631873391c11b3118d
neha <neha@pop-os.localdomain>
2020-08-10 22:17:59 +0530

    added data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
+import "./App.css";
+import "./components/sidebar.css";
+import Sidebar from "./components/sidebar";
+import Header from "./components/navbar";
+im
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Sidebar />
+      
+     
     </div>
   );
 }
 
 export default App;
+{/* <div id="globe" class="globe" style="position: relative; overflow: hidden;">
+  <canvas style="width: 100%; height: 100%;" width="1660" height="505"></canvas>
+  <div style="position: absolute; bottom: 0px; text-shadow: rgb(0, 0, 0) 0px 0px 2px; color: rgb(255, 255, 255); font-size: 10px; padding-right: 5px;">
+    <span class="cesium-credit-imageContainer"></span>
+    <span class="cesium-credit-textContainer">
+      <span class="cesium-credit-text">NASA</span>
+    </span>
+  </div>
+</div>; */}
---
+++ src/components/sidebar.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import { FaTools } from "react-icons/fa";
+
+const Sidebar = () => {
+  return (
+   <div class="sidenav">
+  <a href="#">About</a>
+  <a href="#">Services</a>
+  <a href="#">Clients</a>
+  <a href="#">Contact</a>
+</div>
+
+
+
+  );
+};
+
+
+
+export default Sidebar;
